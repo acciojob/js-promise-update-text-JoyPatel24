8a054f6a57e4812591a1bab2a12de723500c9146
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2023-05-31 17:10:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateText() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve("Hello, world!");
+        }, 1000);
+      });
+    }
+
+    // Update the text after the promise resolves
+    updateText().then((text) => {
+      const outputElement = document.getElementById("output");
+      outputElement.textContent = text;
+    });
